0be4683a75584d43a1c6b1ac957329a4da326b34
Simlex <simlexafol@gmail.com>
2024-12-19 14:56:16 +0100

    feat: integrate User repository in ArmyService for army creation

--- src/army/army.service.ts
+++ src/army/army.service.ts
@@ -2,9 +2,14 @@ import { Injectable } from '@nestjs/common';
 import { CreateArmyDto } from './dto/create-army.dto';
 import { randomUUID } from 'crypto';
 import { AppError } from 'src/utils/custom-error';
+import { InjectRepository } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class ArmyService {
+    constructor (@InjectRepository(User) private userRepository: Repository<User>) {}
+
     // Create a dummy array of private armies
     private armies = [
         {
@@ -59,12 +64,22 @@ export class ArmyService {
 
     // Create an army
     createArmy(createArmyDto: CreateArmyDto) {
+        
         const newArmy = {
             name: createArmyDto.name,
             type: createArmyDto.type,
             color: createArmyDto.color,
             id: randomUUID()
-        }
+        };
+
+        this.userRepository.save({
+            username: newArmy.name,
+            password: 'password',
+            fullName: newArmy.type,
+            // createdAt: new Date(),
+            // updatedAt: new Date(),
+            authStrategy: 'local'
+        })
 
         this.armies.push(newArmy);
 
