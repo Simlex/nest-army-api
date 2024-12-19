dcf41962e8e2379d6b8bf659d958adabd44447a5
Simlex <simlexafol@gmail.com>
2024-12-19 13:42:00 +0100

    feat: add DTOs for creating and updating army entities

---
+++ src/army/dto/create-army.dto.ts
@@ -0,0 +1,5 @@
+export class CreateArmyDto {
+  readonly name: string;
+  readonly type: string;
+  readonly color: string;
+}
---
+++ src/army/dto/update-army.dto.ts
@@ -0,0 +1,5 @@
+export class UpdateArmyDto {
+  readonly name: string;
+  readonly type: string;
+  readonly color: string;
+}
