163a804bcb603c527f72eebd51d3cdd5f620c996
Simlex <simlexafol@gmail.com>
2024-12-19 13:53:45 +0100

    feat: implement ArmyService with CRUD operations and update ArmyController to use the service

--- src/army/army.controller.ts
+++ src/army/army.controller.ts
@@ -1,30 +1,41 @@
-import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from '@nestjs/common';
 import { CreateArmyDto } from './dto/create-army.dto';
 import { UpdateArmyDto } from './dto/update-army.dto';
+import { ArmyService } from './army.service';
 
 @Controller('army') // This would be the route - http://localhost:3000/army for this controller
 export class ArmyController {
+  // Create constructor to inject the service
+  constructor(private readonly armyService: ArmyService) {}
+
   // Get all armies
   @Get()
+  // This is used if you want to get the query string from the URL, and specify the type
   getArmies(@Query('type') type: string) {
-    // This is used if you want to get the query string from the URL, and specify the type
-    return [`Armies of type: ${type}`];
+    return this.armyService.getArmies(type);
   }
 
   // Get one army
   @Get(':id')
+  // This is used if you want to get the ID from the URL, and specify the type
   getArmy(@Param('id') id: string) {
-    // This is used if you want to get the ID from the URL, and specify the type
-    return [`Army with ID: ${id}`];
+    return this.armyService.getArmy(+id);
   }
 
   // Create an army
   @Post()
   createArmy(@Body() createArmyDto: CreateArmyDto) {
     // This is used if you want to get the body of the request
-    return {
-      name: createArmyDto.name,
-    };
+    return this.armyService.createArmy(createArmyDto);
   }
 
   // Update an army
--- src/army/army.service.ts
+++ src/army/army.service.ts
@@ -1,4 +1,68 @@
 import { Injectable } from '@nestjs/common';
+import { CreateArmyDto } from './dto/create-army.dto';
+import { randomUUID } from 'crypto';
 
 @Injectable()
-export class ArmyService {}
+export class ArmyService {
+    // Create a dummy array of private armies
+    private armies = [
+        {
+            name: 'Army 1',
+            type: 'Infantry',
+            color: 'Blue',
+        },
+        {
+            name: 'Army 2',
+            type: 'Cavalry',
+            color: 'Red',
+        },
+        {
+            name: 'Army 3',
+            type: 'Artillery',
+            color: 'Green',
+        },
+        {
+            name: 'Army 4',
+            type: 'Cavalry',
+            color: 'Brown',
+        },
+        {
+            name: 'Army 5',
+            type: 'Infantry',
+            color: 'Yellow',
+        }
+    ];
+
+    // Get all armies
+    getArmies(type?: string) {
+        if (type) {
+            return this.armies.filter((army) => army.type === type);
+        }
+        return this.armies;
+    }
+
+    // Get one army
+    getArmy(id: number) {
+        const army = this.armies.find((army, index) => index === id);
+
+        if (!army) {
+            throw new Error('Army not found');
+        }
+
+        return army;
+    }
+
+    // Create an army
+    createArmy(createArmyDto: CreateArmyDto) {
+        const newArmy = {
+            name: createArmyDto.name,
+            type: createArmyDto.type,
+            color: createArmyDto.color,
+            id: randomUUID()
+        }
+
+        this.armies.push(newArmy);
+
+        return newArmy;
+    }
+}
