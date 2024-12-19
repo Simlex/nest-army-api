1f09758c08d2ecf036c58178ce996d481feddd97
Simlex <simlexafol@gmail.com>
2024-12-19 14:25:56 +0100

    feat: add sample authorization logic to ColonelGuard for route protection

--- src/colonel/colonel.guard.ts
+++ src/colonel/colonel.guard.ts
@@ -10,6 +10,17 @@ export class ColonelGuard implements CanActivate {
   canActivate(
     context: ExecutionContext,
   ): boolean | Promise<boolean> | Observable<boolean> {
+
+    // This is where you would put your logic to check if the user is authorized to access the route
+
+    // Get the request object
+    const request = context.switchToHttp().getRequest();
+
+    // validate the request
+    if (request.headers.authorization) {
+        return true;
+    }
+
     return false; // Set to true to allow access, and false to deny access
   }
 }
