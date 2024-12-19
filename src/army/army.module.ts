386a4bab2c977fda00ce716b700c192c86f985dd
Simlex <simlexafol@gmail.com>
2024-12-19 14:56:11 +0100

    feat: add TypeOrmModule import for User entity in ArmyModule

--- src/army/army.module.ts
+++ src/army/army.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { ArmyController } from './army.controller';
 import { ArmyService } from './army.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([User])],
   controllers: [ArmyController],
-  providers: [ArmyService]
+  providers: [ArmyService],
 })
 export class ArmyModule {}
