29d8e01fcb8e62b798cfd21d957d6499cccc8ecd
Simlex <simlexafol@gmail.com>
2024-12-19 14:56:06 +0100

    feat: configure TypeORM with MySQL for database connection in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,20 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArmyModule } from './army/army.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './typeorm/entities/User';
 
 @Module({
-  imports: [ArmyModule],
+  imports: [ArmyModule, TypeOrmModule.forRoot({
+    type: 'mysql',
+    host: 'localhost',
+    port: 3306,
+    username: 'army-webapp',
+    password: 'password',
+    database: 'army_db',
+    entities: [User],
+    synchronize: true, // Not recommended for production - issues with data loss
+  })],
   controllers: [AppController],
   providers: [AppService],
 })
