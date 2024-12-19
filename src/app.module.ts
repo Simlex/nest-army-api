4c0a9f1b942851d93e155f89c4985c63337293c7
Simlex <simlexafol@gmail.com>
2024-12-19 13:42:07 +0100

    feat: add ArmyModule to the main application module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ArmyModule } from './army/army.module';
 
 @Module({
-  imports: [],
+  imports: [ArmyModule],
   controllers: [AppController],
   providers: [AppService],
 })
