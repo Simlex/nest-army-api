1efb8a2e60be13d596b725c59a42107d54ab0963
Simlex <simlexafol@gmail.com>
2024-12-19 14:19:59 +0100

    feat: add validation rules for name and type in CreateArmyDto

--- src/army/dto/create-army.dto.ts
+++ src/army/dto/create-army.dto.ts
@@ -1,8 +1,13 @@
+import { IsEnum, MinLength } from 'class-validator';
 import { ArmyType } from 'src/enums/ArmyType';
 
 export class CreateArmyDto {
   readonly id: string;
+
+  @MinLength(3) // Adds a validation rule to check if the name is at least 3 characters long
   readonly name: string;
+
+  @IsEnum(ArmyType, { message: 'Use correct type' }) // Adds a validation rule to check if the type is one of the values in the ArmyType enum
   readonly type: ArmyType;
   readonly color: string;
 }
