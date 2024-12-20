93b1a484591c0077704cc18f3713b83b2bcfb3c6
Simlex <simlexafol@gmail.com>
2024-12-20 07:39:09 +0100

    feat: refactor TypeOrmModule configuration to use DatabaseConfig

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ArmyModule } from './army/army.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { User } from './typeorm/entities/User';
+import { DatabaseConfig } from './config/db';
 
 @Module({
-  imports: [ArmyModule, TypeOrmModule.forRoot({
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'army-webapp',
-    password: 'password',
-    database: 'army_db',
-    entities: [User],
-    synchronize: true, // Not recommended for production - issues with data loss
-  })],
+  imports: [ArmyModule, TypeOrmModule.forRoot(DatabaseConfig)],
   controllers: [AppController],
   providers: [AppService],
 })
