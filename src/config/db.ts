cf425a226047158fda4fab96a80954769506eeda
Simlex <simlexafol@gmail.com>
2024-12-25 20:57:41 +0100

    feat: update database configuration to use service hostname

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,13 +1,13 @@
-import { TypeOrmModuleOptions } from "@nestjs/typeorm";
-import { User } from "src/typeorm/entities/User";
+import { TypeOrmModuleOptions } from '@nestjs/typeorm';
+import { User } from 'src/typeorm/entities/User';
 
 export const DatabaseConfig: TypeOrmModuleOptions = {
-    type: 'mysql',
-    host: 'localhost',
-    port: 3306,
-    username: 'army-webapp',
-    password: 'password',
-    database: 'army_db',
-    entities: [User],
-    synchronize: true, // Not recommended for production - issues with data loss
-  }
+  type: 'mysql',
+  host: 'database', // use the service name from docker-compose.yml
+  port: 3306,
+  username: 'army-webapp',
+  password: 'password',
+  database: 'army_db',
+  entities: [User],
+  synchronize: true, // Not recommended for production - issues with data loss
+};
